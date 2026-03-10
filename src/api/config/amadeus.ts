2f0b5b8442957eb7f441f4e99d33128f9db57cec
Shamha-0501 <s_asfer@nexfort.io>
2026-03-10 11:21:38 +0530

    Add Amadeus API client config

---
+++ src/api/config/amadeus.ts
@@ -0,0 +1,8 @@
+"server-only";
+const Amadeus = require("amadeus");
+
+export const amadeus = new Amadeus({
+  clientId: process.env.NEXT_PUBLIC_AMADEUS_CLIENT_ID!,
+  clientSecret: process.env.NEXT_PUBLIC_AMADEUS_CLIENT_SECRET!,
+});
+
