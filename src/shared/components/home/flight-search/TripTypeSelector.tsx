c7dcd6cd0a524ba3d051e393ad443438ea1b8f48
Shamha-0501 <s_asfer@nexfort.io>
2026-03-10 11:22:23 +0530

    Add trip type selector

---
+++ src/shared/components/home/flight-search/TripTypeSelector.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import type { TripType } from "./types";
+
+export default function TripTypeSelector({
+  value,
+  onChange,
+}: {
+  value: TripType;
+  onChange: (v: TripType) => void;
+}) {
+  return (
+    <select
+      value={value}
+      onChange={(e) => onChange(e.target.value as TripType)}
+      className="
+        h-9 rounded-lg
+        bg-transparent text-white
+        border border-white/35
+        px-3 text-sm
+        outline-none
+        focus:ring-2 focus:ring-white/40
+      "
+    >
+      <option className="text-gray-900" value="roundtrip">
+        Roundtrip
+      </option>
+      <option className="text-gray-900" value="oneway">
+        One-way
+      </option>
+    </select>
+  );
+}
