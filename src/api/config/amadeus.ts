796f61267825e09d21c33bb98d4fc367d4fa6958
Shamha-0501 <s_asfer@nexfort.io>
2026-04-27 18:27:28 +0530

    chore(frontend): remove obsolete Amadeus frontend code and config

--- src/api/config/amadeus.ts
+++
@@ -1,8 +0,0 @@
-"server-only";
-const Amadeus = require("amadeus");
-
-export const amadeus = new Amadeus({
-  clientId: process.env.NEXT_PUBLIC_AMADEUS_CLIENT_ID!,
-  clientSecret: process.env.NEXT_PUBLIC_AMADEUS_CLIENT_SECRET!,
-});
-
