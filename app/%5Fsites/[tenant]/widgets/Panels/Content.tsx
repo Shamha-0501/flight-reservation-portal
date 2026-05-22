dffc51df8351fd633956236f5e489550631a18f1
Shamha-0501 <s_asfer@nexfort.io>
2026-05-22 08:44:48 +0530

    deleted some unnecessary files

--- app/%5Fsites/[tenant]/widgets/Panels/Content.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ContentPanel() {
-  return (
-    <div className='w-full h-screen'>ContentPanel</div>
-  )
-}
--- app/%5Fsites/[tenant]/widgets/Panels/Home.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function HomePanel() {
-  return (
-    <div className='w-full h-screen'>HomePanel</div>
-  )
-}
