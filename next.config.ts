e51470540e80553be9a47c6e5cfc14536bf6b1ae
Shamha-0501 <s_asfer@nexfort.io>
2026-07-13 00:23:48 +0530

    added docker

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   allowedDevOrigins: ['lvh.me', '*.lvh.me', 'localhost', '*.localhost'],
   images: {
     remotePatterns: [
